a3b78d0df89688dc1b3424c3afc50d0f4befa237
github username <um_2003_8@hotmail.com>
2020-12-01 17:57:59 -0500

    add initial state, add value & onChange to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    state = { 
+        input: "" 
+    }
+
     render() {
         return(
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Search Video</label>
-                        <input type="text"/>
+                        <input 
+                            type="text" 
+                            value={this.state.input} 
+                            onChange={this.handleOnChange}
+                            />
                     </div>
                 </form>
             </div>
