361cb25ff45d6c0dc7d79b9440ad45d0ce184ca4
github username <um_2003_8@hotmail.com>
2020-12-01 17:43:44 -0500

    create form. add input and label then classNames

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,14 @@ import React from 'react'
 class SearchBar extends React.Component {
     render() {
         return(
-            <div>SearchBar</div>
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Search Video</label>
+                        <input type="text"/>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
