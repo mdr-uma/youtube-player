51f2e728ab589ae2272f6fd747d6032a3daec480
github username <um_2003_8@hotmail.com>
2020-12-02 19:02:28 -0500

    add props to SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     render() {
         return(
             <div className="ui container">
-                <SearchBar />
+                <SearchBar onFormSubmit={this.onInputSubmit}/>
             </div>
         )
     }
