77d2e51b834cd8f4ef913960c1ad3f9c269cc9a6
github username <um_2003_8@hotmail.com>
2020-12-02 21:05:57 -0500

    moved params and key from youtube.js to App.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,5 @@
-import axios from 'axios'
-
-const KEY = 'AIzaSyBx0J-rnLChsu4UTVJX227iXqr1Z9ZcCeI'
+import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 8,
-        key: KEY
-    }
-
+    baseURL: "https://www.googleapis.com/youtube/v3"
 })
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import youtube from '../apis/youtube'
+
+const KEY = 'AIzaSyBx0J-rnLChsu4UTVJX227iXqr1Z9ZcCeI'
 
 class App extends React.Component {
+    onInputSubmit = async input => {
+        const res = await youtube.get('/search', {
+            params: {
+                q: input,
+                part: 'snippet',
+                type: 'video',
+                maxResults: 8,
+                key: KEY
+            }
+        })
+        console.log(res.data.items);
+    }
+
     render() {
         return(
             <div className="ui container">
