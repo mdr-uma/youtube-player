e83db9f8193dfc6a09b90b9093f50cde2c807d00
github username <um_2003_8@hotmail.com>
2020-12-03 21:15:55 -0500

    add new state & callback function to update state

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 const KEY = 'AIzaSyBx0J-rnLChsu4UTVJX227iXqr1Z9ZcCeI'
 
 class App extends React.Component {
     state = { 
-        videos: [] 
+        videos: [],
+        selectedVideo: null 
     }
 
     onInputSubmit = async input => {
@@ -26,11 +28,18 @@ class App extends React.Component {
         })
     }
 
+    onVideoSelect = (video) => {
+        this.setState({
+            selectedVideo: video
+        })
+    }
+
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onInputSubmit}/>
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         )
     }
